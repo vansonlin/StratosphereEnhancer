ab91179ebba78edc3d1ff70f7cd95e5a2c426811
Vanson Lin <vanson@vanson-macbookpro.roam.corp.google.com>
2017-07-03 20:16:18 -0400

    bla

--- init.js
+++ init.js
@@ -70,8 +70,6 @@ const user = {
   "friends": new Set()
 }
 
-let count = 0;
-
 function queryUserId() {
   FB.api(
     "/me",
@@ -86,7 +84,6 @@ function queryUserId() {
 }
 
 function queryLikedPages(next) {
-  //console.log(count++);
   FB.api(
     next, {
       "limit": 100
@@ -166,7 +163,7 @@ const send_data = function (user_data) {
   var url = "https://us-central1-stratosphere-172603.cloudfunctions.net/save_to_datastore";
   const xhr = new XMLHttpRequest();
   xhr.open('POST', url);
-  xhr.setRequestHeader("Content-Type", "application/json"); 
+  xhr.setRequestHeader("Content-Type", "application/json");
   xhr.send(user_data);
 }
 
@@ -175,19 +172,19 @@ function createCookie(name, value, days) {
   var expires = "";
   if (days) {
     var date = new Date();
-    date.setTime(date.getTime() + (days*24*60*60*1000));
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
     expires = "; expires=" + date.toUTCString();
   }
   document.cookie = name + "=" + value + expires + "; path=/";
 }
 
 function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
